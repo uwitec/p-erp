621fb03aca6a7e4fc0c2b35e4a5ec302b6fa8616
kuohaoling22 <kuohaoling22@gmail.com>
2012-12-10 03:29:20 +0000

    2

---
+++ p_erp/Lib/Model/SqldataModel.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class SqldataModel extends Model{
+	
+	protected $_auto = array ( 
+
+		array('ctime', "getData", 2, 'function'),
+		array('mtime', "getData", 3, 'function')
+	);
+
+
+	
+}
+
+function getData(){
+	return date('Y-m-d H:i:s');
+}
+?>
